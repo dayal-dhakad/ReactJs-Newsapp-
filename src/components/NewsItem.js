8994e5dd7e662e02a6142375cdecd7389a739a76
Deendayal Dhakad <dayal.dhakad.125@gmail.com>
2023-04-22 15:55:48 +0530

    final commit for news app

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -10,8 +10,8 @@ export class NewsItem extends Component {
          
         <span className="position-absolute top-0translate-middle badge rounded-pill bg-danger" style={{zIndex:'1', left:'80%'}}>
            {source} </span>
-          <img src= {imageUrl} className="card-img-top" style={{height:"250px"}} alt="..." />
-          <div className="card-body">
+           <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" style={{height:"250px"}} alt="..." />  
+                   <div className="card-body">  
             <h5 className="card-title">{title}... </h5>
             <p className="card-text">{description}...</p>
             <p className="card-text"><small className="text-muted"> By {!author?"Unknown":author} on {new Date(date).toGMTString()}  </small></p>
